36d3d92ebd71f9c8994f1e18e046a7c00cd53217
EJG <gavis.edwin@gmail.com>
2020-05-30 19:33:07 -0500

    less wordy

--- destiny/markov.js
+++ destiny/markov.js
@@ -1,6 +1,6 @@
-fetch('https://raw.githubusercontent.com/jamiegavis/destiny-lore/master/model.json')
+var model = fetch('https://raw.githubusercontent.com/jamiegavis/destiny-lore/master/model.json')
   .then(response => response.json())
-  .then(data => var model = data);
+
 
 var c_dist = 0 
 
