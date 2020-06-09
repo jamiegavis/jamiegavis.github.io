956e90ced69a04762c0f7ae711e536ca4ce6a05e
EJG <gavis.edwin@gmail.com>
2020-06-08 19:42:01 -0500

    added basic walk & step functionality

--- destiny/markov.js
+++ destiny/markov.js
@@ -1,25 +1,41 @@
-var model,c_dist,ranGen,i
 var markov = new Object();
+var exported = 'https://raw.githubusercontent.com/jamiegavis/destiny-lore/master/model.json';
 
-fetch('https://raw.githubusercontent.com/jamiegavis/destiny-lore/master/model.json')
+fetch(exported)
 	.then(response => response.json())
 	.then(data => JSON.parse(data["chain"]))
 	.then(model => buildMarkov(model));
+	//.then(_ => sentence.push(step("___BEGIN__,___BEGIN__"))) works but promises issue
 
-ranGen = (maximum) => Math.ceil(Math.random()*maximum);
+var ranInt = (maximum) => Math.floor(Math.random()*maximum);
 
-buildMarkov = (m) => { 
-	for (i = 0; i < m.length; i++) { 
-	    markov[m[i][0]] = markov[m[i][1]] 
+var buildMarkov = (m) => { 
+	var rolling
+	for (let i = 0; i < m.length; i++) {
+		rolling = 0 
+		markov[m[i][0]] = []
+		for (let [key,value] of Object.entries(m[i][1])) {
+			rolling += value
+			markov[m[i][0]].push([rolling, key])	//memory concerns?		
+		}
 	}
+}; 
+
+var step = (state) => {
+	var rand = ranInt(markov[state][markov[state].length-1][0])
+	for (let i = 0; i < markov[state].length; i++) {
+		if (rand < markov[state][i][0]) return markov[state][i][1]
+	}	 
+};
+
+var walk = (start) => {
+	var sentence = ["___BEGIN__"]
+	sentence.push(step(start))
+	while (sentence[sentence.length-1] !== "___END__") {
+		console.log(sentence)
+		var state = sentence.slice(sentence.length-2).join(",")
+		sentence.push(step(state))		
+	}	
+	return sentence
 } 
-
-/*
-Build a Python representation of the Markov model. Returns a dict
-of dicts where the keys of the outer dict represent all possible states,
-and point to the inner dicts. The inner dicts represent all possibilities
-for the "next" item in the chain, along with the count of times it
-appears.
-*/
-
 
